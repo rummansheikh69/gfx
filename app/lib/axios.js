97208ffd7dc1abade99735d8dc22d1add1f49862
Rumman Sheikh <rummansheikh2004@gmail.com>
2025-11-28 02:18:09 +0600

    Change axios base URL to production endpoint

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://gfxbackend.onrender.com/api",
   withCredentials: true,
 });
